ce65925f33e75390dd45dfc77b508280e5090d92
AdityaDwiNugroho <veldora5000@gmail.com>
2024-10-10 01:03:03 +0700

    fix: typescript explicit any

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 if (process.env.REACT_DEV_TOOLS === "true") {
   console.log("React DevTools are enabled");
 } else {
-  (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ = undefined;
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = undefined;
 }
 
 export default function Home() {
