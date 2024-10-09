eee9c768877f6c4ddc44f58263f8eb1e18fefc30
AdityaDwiNugroho <veldora5000@gmail.com>
2024-10-10 01:07:38 +0700

    fix: undefined window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import "./globals.css";
 import Hero from "./components/Hero";
-
-if (process.env.REACT_DEV_TOOLS === "true") {
-  console.log("React DevTools are enabled");
-} else {
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = undefined;
-}
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (process.env.REACT_DEV_TOOLS === "true") {
+      console.log("React DevTools are enabled");
+    } else {
+      (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ = undefined;
+    }
+  }, []);
+
   return (
     <div>
       <Hero />
